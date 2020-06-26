a60fa29d37b01a4e8a69ad45c1451d537ecf360e
Thiago Lima <thitheguy@gmail.com>
2020-06-25 21:26:10 -0300

    Consuming data from the GraphQL Back-end through Apollo-Client on the Front-End

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('GraphQL Apollo Client API to be built')
+import ApolloBoost, { gql } from  'apollo-boost'
+
+const client = new ApolloBoost({
+    uri: 'http://localhost:4000'
+})
+
+const getUsers = gql `
+     query {
+         users {
+             id
+             name
+         }
+     }
+`
+
+client.query({
+    query: getUsers
+}).then((res) => {
+    console.log(res.data)
+})
